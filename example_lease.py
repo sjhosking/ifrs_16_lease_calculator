f409a4911c8d4662c9ce73804d42297f675e469b
Scott Hosking <46496122+sjhosking@users.noreply.github.com>
2024-05-06 19:35:15 +0930

    Update example_lease.py

--- example_lease.py
+++ example_lease.py
@@ -1,32 +1,26 @@
 import datetime
 from lease_calculator import lease_calculator
 import pandas as pd
+import streamlit as st
+
 # Input parameter
-payment_amount = 1000  # Initial payment
-period_increase_rate = 0  # Annual increase rate in percent
-period_increase_frequency = 0
-num_periods = 12 # Number of periods
-discount_rate = 0.07  # Discount rate (5% in this example)
-first_payment_date = datetime.date(2021,1,1)
-contract_start_date = datetime.date(2021,1,1)
-roua_start_date = datetime.date(2021,1,1)
-payment_frequency = 'M'
-dep_method = 'S'
-dim_factor = 2
-roua_direct_costs = 0
-roua_dismantling_costs = 0
-period_start_end_date = [[datetime.date(2021,1,1), datetime.date(2021,1,31)],
-                        [datetime.date(2021,2,1), datetime.date(2021,2,28)],
-                        [datetime.date(2021,3,1), datetime.date(2021,3,31)],
-                        [datetime.date(2021,4,1), datetime.date(2021,4,30)],
-                        [datetime.date(2021,5,1), datetime.date(2021,5,31)],
-                        [datetime.date(2021,6,1), datetime.date(2021,6,30)],
-                        [datetime.date(2021,7,1), datetime.date(2021,7,31)],
-                        [datetime.date(2021,8,1), datetime.date(2021,8,31)],
-                        [datetime.date(2021,9,1), datetime.date(2021,9,30)],
-                        [datetime.date(2021,10,1), datetime.date(2021,10,31)],
-                        [datetime.date(2021,11,1), datetime.date(2021,11,30)],
-                        [datetime.date(2021,12,1), datetime.date(2021,12,31)]]
+
+first_payment_date = st.date_input("First Payment Date", datetime.date(2021, 1, 1))
+contract_start_date = st.date_input("Contract Start Date", datetime.date(2021, 1, 1))
+roua_start_date = st.date_input("ROUA Start Date", datetime.date(2021, 1, 1))
+payment_amount = st.number_input("Payment Amount",1000)
+period_increase_rate = st.number_input("Payment Increase Rate",0)
+period_increase_frequency = st.number_input("Payment Increase Frequency",)
+num_periods = st.number_input('Number of Periods',12)
+discount_rate = st.number_input("Discount Rate",0.05)
+payment_frequency = st.text_input("Payment Frequency",'M')
+dep_method = st.text_input("Depreciation Method",'S')
+dim_factor = st.number_input("Diminishing Value Dim Factor",2)
+
+roua_direct_costs = st.number_input("ROUA Direct Costs",0)
+roua_dismantling_costs = st.number_input("ROUA Dismantling Costs",0)
+
+period_start_end_date = [[st.date_input('Report Start Date',datetime.date(2021,6,1)), st.date_input('Report Start Date',datetime.date(2021,6,30))]]
 period_end_date = datetime.date(2021,12,31)
 eofy = datetime.date(2021,6,30)
 
@@ -46,6 +40,12 @@ lease_info = lease_calculator(payment_amount,
                                             eofy)
 
 daily_schedule = lease_info.calculate_daily_schedule()
+st.title('IFRS Period Calculator')
+
 period = lease_info.period_report(period_start_end_date)
 df = pd.DataFrame.from_dict(period)
-df.to_csv('lease.csv')
+st.dataframe(df)
+
+st.title('IFRS16 Daily Schedule')
+df = pd.DataFrame.from_dict(daily_schedule)
+st.dataframe(df)
